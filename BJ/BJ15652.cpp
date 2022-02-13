d2a5d612c8b4b7ffd7442a963f15002e0472d1f7
ChangMini <dlwlxns4@gmail.com>
2022-02-13 19:37:16 +0900

    BJ15652

---
+++ BJ/BJ15652.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+vector<int> ar;
+vector<int> result;
+
+void DFS()
+{
+	if (result.size() == M)
+	{
+		for (auto i : result)
+		{
+			cout << i << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 0; i < ar.size(); ++i)
+	{
+		if (result.empty() || result.back() <= ar[i])
+		{
+			result.push_back(ar[i]);
+			DFS();
+			result.pop_back();
+		}
+	}
+
+}
+
+int main() {
+	cin >> N >> M;
+
+	for (int i = 0; i < N; ++i)
+	{
+		ar.push_back(i + 1);
+	}
+
+
+	DFS();
+}
