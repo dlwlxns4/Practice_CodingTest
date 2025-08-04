68a08e1933046e2ce6fb37e5d348287a81f76c44
ChangMini <dlwlxns4@gmail.com>
2025-08-05 03:40:01 +0900

    BJ11722 : 가장 긴 감소하는 부분 수열

---
+++ BJ/BJ11722.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+
+int N;
+vector<int> A;
+vector<int> Dp;
+vector<int> Prev;
+
+void Input();
+void Solution();
+
+void Input()
+{
+	cin >> N;
+	A.resize(N);
+	Dp.resize(N);
+	Prev.resize(N);
+
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> A[i];
+		Dp[i] = 1;
+		Prev[i] = -1;
+	}
+}
+
+void Solution()
+{
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < i; ++j)
+		{
+			if (A[i] < A[j])
+			{
+				Dp[i] = max(Dp[i], Dp[j] + 1);
+			}
+		}
+	}
+
+	cout << *max_element(Dp.begin(), Dp.end());
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	Input();
+	Solution();
+}
