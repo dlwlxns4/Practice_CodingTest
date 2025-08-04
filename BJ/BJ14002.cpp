d6a7aa9de5b23b9dfcc3eab7f117876348a9cabb
ChangMini <dlwlxns4@gmail.com>
2025-08-04 20:53:01 +0900

    BJ14002 : 가장 긴 증가하는 부분 수열 4

---
+++ BJ/BJ14002.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+
+int N;
+vector<int> A;
+vector<int> Dp;
+vector<int> Prev;
+
+void Input();
+void Solution();
+
+void Input()
+{
+	cin >> N;
+	A.resize(N);
+	Dp.resize(N);
+	Prev.resize(N);
+
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> A[i];
+		Dp[i] = 1;
+		Prev[i] = -1;
+	}
+}
+
+void Solution()
+{
+	int maxLen = 0;
+	int lastIdx = 0;
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < i; ++j)
+		{
+			if (A[j] < A[i] && Dp[i] < Dp[j] + 1)
+			{
+				Dp[i] = Dp[j] + 1;
+				Prev[i] = j;
+			}
+		}
+
+		if (Dp[i] > maxLen) 
+		{
+			maxLen = Dp[i];
+			lastIdx = i;
+		}
+
+	}
+
+	vector<int> result;
+	for (int cur = lastIdx; cur != -1; cur = Prev[cur]) 
+	{
+		result.push_back(A[cur]);
+	}
+
+	reverse(result.begin(), result.end());
+
+	cout << result.size() << "\n";
+	for(auto index : result)
+	{
+		cout << index << " ";
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	Input();
+	Solution();
+}
